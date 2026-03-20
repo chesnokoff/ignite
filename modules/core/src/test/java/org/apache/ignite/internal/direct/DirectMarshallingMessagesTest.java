87683ef82b0c6851938306ffe01ba546e3fa3b44
chesnokoff <chesnokoff239@gmail.com>
2026-03-21 00:23:54 +0300

    IGNITE-28294 Fix test

--- modules/core/src/test/java/org/apache/ignite/internal/direct/DirectMarshallingMessagesTest.java
+++ modules/core/src/test/java/org/apache/ignite/internal/direct/DirectMarshallingMessagesTest.java
@@ -22,10 +22,8 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
-import java.util.function.Function;
 import org.apache.ignite.internal.managers.communication.GridIoMessageFactory;
 import org.apache.ignite.internal.managers.communication.IgniteMessageFactoryImpl;
-import org.apache.ignite.internal.util.distributed.SingleNodeMessage;
 import org.apache.ignite.internal.util.typedef.internal.U;
 import org.apache.ignite.plugin.extensions.communication.Message;
 import org.apache.ignite.plugin.extensions.communication.MessageArrayType;
@@ -41,7 +39,6 @@ import org.apache.ignite.plugin.extensions.communication.MessageWriter;
 import org.apache.ignite.testframework.junits.common.GridCommonAbstractTest;
 import org.junit.Test;
 
-import static org.apache.ignite.internal.util.distributed.DistributedProcess.DistributedProcessType.TEST_PROCESS;
 import static org.apache.ignite.marshaller.Marshallers.jdk;
 import static org.junit.Assert.assertArrayEquals;
 
@@ -65,21 +62,6 @@ public class DirectMarshallingMessagesTest extends GridCommonAbstractTest {
                 new NestedContainersMessageSerializer()
             )
         });
-
-    /** */
-    @Test
-    public void testSingleNodeMessage() {
-        SingleNodeMessage<?> srcMsg =
-            new SingleNodeMessage<>(UUID.randomUUID(), TEST_PROCESS, "data", new Exception("error"));
-
-        SingleNodeMessage<?> resMsg = doMarshalUnmarshal(srcMsg);
-
-        assertEquals(srcMsg.type(), resMsg.type());
-        assertEquals(srcMsg.processId(), resMsg.processId());
-        assertEquals(srcMsg.response(), resMsg.response());
-        assertEquals(srcMsg.error().getClass(), resMsg.error().getClass());
-        assertEquals(srcMsg.error().getMessage(), resMsg.error().getMessage());
-    }
 
     /** */
     @Test
@@ -107,35 +89,6 @@ public class DirectMarshallingMessagesTest extends GridCommonAbstractTest {
         assertEquals(msg.nestedCollection, resMsg.nestedCollection);
         assertArrayEquals(msg.nestedArray.get(1), resMsg.nestedArray.get(1));
         assertArrayEquals(msg.nestedArray.get(2), resMsg.nestedArray.get(2));
-    }
-
-    /**
-     * @param srcMsg Message to marshal.
-     * @param <T> Message type.
-     * @return Unmarshalled message.
-     */
-    private <T extends Message> T doMarshalUnmarshal(T srcMsg) {
-        ByteBuffer buf = ByteBuffer.allocate(8 * 1024);
-
-        boolean fullyWritten = loopBuffer(buf, 0, buf0 -> srcMsg.writeTo(buf0, new DirectMessageWriter(msgFactory)));
-        assertTrue("The message was not written completely.", fullyWritten);
-
-        buf.flip();
-
-        byte b0 = buf.get();
-        byte b1 = buf.get();
-
-        short type = (short)((b1 & 0xFF) << 8 | b0 & 0xFF);
-
-        assertEquals(srcMsg.directType(), type);
-
-        T resMsg = (T)msgFactory.create(type);
-
-        boolean fullyRead = loopBuffer(buf, buf.position(),
-            buf0 -> resMsg.readFrom(buf0, new DirectMessageReader(msgFactory, null)));
-        assertTrue("The message was not read completely.", fullyRead);
-
-        return resMsg;
     }
 
     /**
@@ -190,27 +143,6 @@ public class DirectMarshallingMessagesTest extends GridCommonAbstractTest {
         }
 
         return (T)resMsg;
-    }
-
-    /**
-     * @param buf Byte buffer.
-     * @param start Start position.
-     * @param func Function that is sequentially executed on a different-sized part of the buffer.
-     * @return {@code True} if the function returns {@code True} at least once, {@code False} otherwise.
-     */
-    private boolean loopBuffer(ByteBuffer buf, int start, Function<ByteBuffer, Boolean> func) {
-        int pos = start;
-
-        do {
-            buf.position(start);
-            buf.limit(++pos);
-
-            if (func.apply(buf))
-                return true;
-        }
-        while (pos < buf.capacity());
-
-        return false;
     }
 
     /** */
